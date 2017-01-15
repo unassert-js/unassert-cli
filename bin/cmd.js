a02b62f7a2f3494b6f100c6f5bd51ad01b06a29e
Takuto Wada <takuto.wada@gmail.com>
2017-01-16 02:16:32 +0900

    chore(license): update copyright year

--- bin/cmd.js
+++ bin/cmd.js
@@ -8,7 +8,7 @@
  *
  * https://github.com/unassert-js/unassert-cli
  *
- * Copyright (c) 2016 Takuto Wada
+ * Copyright (c) 2016-2017 Takuto Wada
  * Licensed under the MIT license.
  *   https://github.com/unassert-js/unassert-cli/blob/master/LICENSE
  */
