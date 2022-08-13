233baaf3d4f09235936fec16f3ad767030607671
Takuto Wada <takuto.wada@gmail.com>
2022-08-13 12:06:25 +0900

    refactor: modernize test codebase

--- test/test.js
+++ test/test.js
@@ -1,42 +1,42 @@
 'use strict';
 
-const child = require('child_process');
-const fs = require('fs');
-const path = require('path');
+const { spawn } = require('child_process');
+const { readFileSync } = require('fs');
+const { join, resolve } = require('path');
 const concat = require('concat-stream');
-const assert = require('assert');
-const unassertCommand = path.join(__dirname, '..', 'bin', 'cmd.js');
+const assert = require('assert').strict;
+const unassertCommand = join(__dirname, '..', 'bin', 'cmd.js');
 
 function testUnassertCommand (fixtureName) {
-  const fixtureFilepath = path.resolve(__dirname, 'fixtures', fixtureName, 'fixture.js');
-  const expectedFilepath = path.resolve(__dirname, 'fixtures', fixtureName, 'expected.js');
-  const input = fs.readFileSync(fixtureFilepath, 'utf8');
-  const expected = fs.readFileSync(expectedFilepath, 'utf8');
+  const fixtureFilepath = resolve(__dirname, 'fixtures', fixtureName, 'fixture.js');
+  const expectedFilepath = resolve(__dirname, 'fixtures', fixtureName, 'expected.js');
+  const input = readFileSync(fixtureFilepath, 'utf8');
+  const expected = readFileSync(expectedFilepath, 'utf8');
   function assertOutputMatches (proc, done) {
-    proc.stdout.pipe(concat(function (output) {
+    proc.stdout.pipe(concat((output) => {
       assert.equal(output.toString('utf8'), expected);
       done();
     }));
   }
-  describe(fixtureName, function () {
-    it('open file when filepath is specified', function (done) {
-      const proc = child.spawn(unassertCommand, [fixtureFilepath]);
+  describe(fixtureName, () => {
+    it('open file when filepath is specified', (done) => {
+      const proc = spawn(unassertCommand, [fixtureFilepath]);
       assertOutputMatches(proc, done);
     });
-    it('read from stdin when filepath is not specified', function (done) {
-      const proc = child.spawn(unassertCommand);
+    it('read from stdin when filepath is not specified', (done) => {
+      const proc = spawn(unassertCommand);
       assertOutputMatches(proc, done);
       proc.stdin.end(input);
     });
-    it('read from stdin when filepath is "-"', function (done) {
-      const proc = child.spawn(unassertCommand, ['-']);
+    it('read from stdin when filepath is "-"', (done) => {
+      const proc = spawn(unassertCommand, ['-']);
       assertOutputMatches(proc, done);
       proc.stdin.end(input);
     });
   });
 }
 
-describe('unassert-cli', function () {
+describe('unassert-cli', () => {
   testUnassertCommand('func');
   testUnassertCommand('commonjs');
   testUnassertCommand('commonjs_singlevar');
